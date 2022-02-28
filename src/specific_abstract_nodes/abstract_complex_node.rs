a8a73f3a12058367b5b0723d424083f99387f97f
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-28 15:09:42 +0800

    Add abstraction of complex node

---
+++ src/specific_abstract_nodes/abstract_complex_node.rs
@@ -0,0 +1,20 @@
+/// An abstraction of complex node.
+pub trait AbstractComplexNode {
+	/// The type that represents the complex' name.
+	type Complex;
+
+	/// The type that represents the complex' attacher(s).
+	type Attachers;
+
+	/// The type that represents the complex' nodes(s).
+	type Nodes;
+
+	/// Returns the name of the complex node.
+	fn name(&self) -> &Self::Complex;
+
+	/// Returns the attachers of the complex node.
+	fn attachers(&self) -> &Self::Attachers;
+
+	/// Returns the nodes in the complex node.
+	fn nodes(&self) -> &Self::Nodes;
+}
