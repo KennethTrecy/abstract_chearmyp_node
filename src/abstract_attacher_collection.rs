42f2711051724ce5f678780b8d2d058b3c348a40
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-28 15:11:17 +0800

    Add abstraction of attacher collection

---
+++ src/abstract_attacher_collection.rs
@@ -0,0 +1,17 @@
+/// An abstraction of Chearmyp attacher collection.
+pub trait AbstractAttacherCollection<T, U> {
+	/// Creates an empty attacher collection.
+	fn new() -> Self;
+
+	/// Finds the mutable attacher.
+	fn get(&self, _: T) -> Option<&U>;
+
+	/// Finds the mutable attacher.
+	fn get_mut(&mut self, _: T) -> Option<&mut U>;
+
+	/// Puts the attacher into the attacher collection.
+	fn attach(&mut self, _: U);
+
+	/// Takes the attacher in the attacher collection.
+	fn deattach(&mut self, _: T) -> Option<U>;
+}
