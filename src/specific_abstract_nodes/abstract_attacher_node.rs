56c6c861ee68b5f2c16c4b76cddcd09bc3c726f2
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-28 15:07:55 +0800

    Add abstraction of attacher node

---
+++ src/specific_abstract_nodes/abstract_attacher_node.rs
@@ -0,0 +1,20 @@
+/// An abstraction of attacher node.
+pub trait AbstractAttacherNode {
+	/// The type of label in the attacher node.
+	type Label;
+
+	/// The type of content in the attacher node.
+	type Content;
+
+	/// The type of comments in the attacher node.
+	type Comments;
+
+	/// Returns the label of the attacher.
+	fn label(&self) -> &Self::Label;
+
+	/// Returns the content of the attacher.
+	fn content(&self) -> &Self::Content;
+
+	/// Returns the comments to the attacher.
+	fn comments(&self) -> &Self::Comments;
+}
